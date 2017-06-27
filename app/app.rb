c2201b6cd82d294da61355ebd36b16327ffb5995
Jasper King-Harman <j.kingharman@googlemail.com>
2017-06-27 12:40:45 +0100

    Adding links controller and related view

--- app/app.rb
+++ app/app.rb
@@ -3,10 +3,32 @@ require 'sinatra/base'
 require_relative './models/link.rb'
 
 class App < Sinatra::Base
-  
-  
-  get '/' do
-    
+
+  enable :sessions
+
+  configure do
+    # Don't log them. We'll do that ourself
+    set :dump_errors, false
+
+    # Don't capture any errors. Throw them up the stack
+    set :raise_errors, true
+
+    # Disable internal middleware for presenting errors
+    # as useful HTML pages
+    set :show_exceptions, false
   end
-  
+
+  #set :views, Proc.new{File.join(root, "..", "views")}
+
+  get '/' do
+
+  end
+
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
+  end
+
+  run! if app_file == $0
+
 end
--- spec/features/home/links_spec.rb
+++ spec/features/home/links_spec.rb
@@ -3,7 +3,7 @@
 feature 'ability to see boomark links' do
   scenario 'user visits home page' do
     Link.create(url: 'http://www.makersacademy.com', title: 'Linky')
-    visit('/')
+    visit('/links')
     expect(page).to have_content("Linky")
   end
 end
