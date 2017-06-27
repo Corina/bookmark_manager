c5f0ee06db00c1e1de4892cc1bc7fe6ea79f704e
Corina <fluturelcorina@gmail.com>
2017-06-27 13:37:50 +0100

    Creating links feature added

---
+++ spec/features/links/creating_links_spec.rb
@@ -0,0 +1,11 @@
+
+
+feature 'ability to add a link' do
+  scenario 'user adds a link' do
+    visit('/links/new')
+    fill_in('title', :with => 'Google' )
+    fill_in('url', :with => 'https://www.google.com/' )
+    click_on('Submit')
+    expect(Link.last.url).to eq 'https://www.google.com/'
+  end
+end
