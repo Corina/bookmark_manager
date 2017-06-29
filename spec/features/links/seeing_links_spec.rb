c8f88c1555b77700939d4f868d17637cf64f0ba5
Corina <fluturelcorina@gmail.com>
2017-06-29 01:17:47 +0100

    Filtering by tag

--- spec/features/links/seeing_links_spec.rb
+++ spec/features/links/seeing_links_spec.rb
@@ -1,7 +1,26 @@
-feature 'ability to see bookmark links' do
+feature 'ability to see  links' do
+  
+  before (:each) do
+    Link.create(title: 'wiki ice', url: 'https://en.wikipedia.org/wiki/Ice', tags: [Tag.first_or_create(name: 'types of ice')])
+    Link.create(title: 'vonnegut', url: 'https://www.vonnegut.com/', tags: [Tag.first_or_create(name: 'author')])
+    Link.create(title: 'tate', url: 'http://www.tate.org.uk/visit/tate-modern', tags: [Tag.first_or_create(name: 'museum')])
+    Link.create(title: 'Bubbledogs', url: 'https://www.bubbledogs.co.uk/', tags: [Tag.first_or_create(name: 'bubbledogs')])
+  end
+  
   scenario 'user visits home page' do
-    Link.create(url: 'http://www.makersacademy.com', title: 'Linky')
     visit('/links')
-    expect(page).to have_content("Linky")
+    expect(page).to have_content("wiki ice")
+  end
+  
+  scenario 'can filter by links' do
+    visit 'tags/bubbledogs'
+    
+    expect(page.status_code).to eq(200)
+    within 'ul#links' do
+      expect(page).not_to have_content('wiki ice')
+      expect(page).not_to have_content('vonnegut')
+      expect(page).not_to have_content('tate')
+      expect(page).to have_content('bubbledogs')
+    end
   end
 end
